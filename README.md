d853c3c1253a604794d15b418b7b2c7d482cd9ac
Lucas Hahmann <lucashahmann@gmail.com>
2021-10-23 23:12:34 +0200

    Change to flask

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Own-Mail-System
-A Mail System just for me.
+A Mail System just for me. 
+
+Backend> Python API with Flask Restful
+Frontend> VueJS
+
+By CDO
--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,19 @@
-# Imports.....
-import falcon
-from wsgiref.simple_server import make_server
+# Imports
+from flask import Flask
+from flask_restful import Resource, Api
 
-# Global Variables
+# Init Flask
+app = Flask(__name__)
+api = Api(app)
 
-PORT = 300
+# Class
+class HelloFlask(Resource):
+    def get(self):
+        return {'hello': 'flask'}
 
-# Classes from Routes
-class ThingsResource:
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
-        resp.text = ('Hello, World!')
-    def on_post(self, req, resp):
-        print(req)
+# Add Routes
+api.add_resource(HelloFlask, '/')
 
-
-# Init Falcon
-app = falcon.App()
-
-# Configure class
-things = ThingsResource()
-
-# Configure Routes
-app.add_route('/things', things)
-
+# Start the engine
 if __name__ == '__main__':
-    with make_server('', PORT, app) as httpd:
-        print(f'Serving on port {PORT}...')
-
-        # Serve until process is killed
-        httpd.serve_forever()
+    app.run(debug=True)
