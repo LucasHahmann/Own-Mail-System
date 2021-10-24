30a90684d7ca578b52bcb7f31bd429162879f2fc
Lucas Hahmann <lucashahmann@gmail.com>
2021-10-24 11:07:03 +0200

    First Endpoint, getMails

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+import mail
+
 # Init Flask
 app = Flask(__name__)
 api = Api(app)
@@ -10,9 +12,19 @@ api = Api(app)
 class HelloFlask(Resource):
     def get(self):
         return {'hello': 'flask'}
+    def post(self):
+        print(self)
+
+class GetEmails(Resource):
+    def post(self, username, password):
+        mail.getMails(username, password)
+        f = open("mail.json", "r")
+        data = f.read()
+        return data
 
 # Add Routes
 api.add_resource(HelloFlask, '/')
+api.add_resource(GetEmails, '/getEmails/<string:username>/<string:password>')
 
 # Start the engine
 if __name__ == '__main__':
